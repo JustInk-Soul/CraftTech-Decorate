68c9a05c742ea20a11df0ae99005117c5ea88847
Just Ink Soul <90371792+JustInk-Soul@users.noreply.github.com>
2022-10-04 22:03:56 +0800

    Add files via upload

--- src/main/java/inksoul/MyModDatagen.java
+++ src/main/java/inksoul/MyModDatagen.java
@@ -2,6 +2,7 @@ package inksoul;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
@@ -11,11 +12,13 @@ public class MyModDatagen implements DataGeneratorEntrypoint {
 
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		fabricDataGenerator.addProvider(MyTagGenerator::new);
+		fabricDataGenerator.addProvider(MyModelGenerator::new);
+		fabricDataGenerator.addProvider(EnglishLanguageGenerator::new);
+		fabricDataGenerator.addProvider(ChineseLanguageGenerator::new);
 	}
 
-	private static class MyTagGenerator extends FabricModelProvider {
-		public MyTagGenerator(FabricDataGenerator dataGenerator) {
+	private static class MyModelGenerator extends FabricModelProvider {
+		public MyModelGenerator(FabricDataGenerator dataGenerator) {
 			super(dataGenerator);
 		}
 
@@ -30,5 +33,35 @@ public class MyModDatagen implements DataGeneratorEntrypoint {
 				itemModelGenerator.register(i, Models.GENERATED);
 			}
 		};
+	}
+
+	private static class EnglishLanguageGenerator extends FabricLanguageProvider {
+		private EnglishLanguageGenerator(FabricDataGenerator dataGenerator) {
+			// Specifying en_us is optional, by default is is en_us.
+			super(dataGenerator, "en_us");
+		}
+
+		@Override
+		public void generateTranslations(TranslationBuilder translationBuilder) {
+			for (var i : ExampleMod.iiitem) {
+				translationBuilder.add(i, "Machine Block");
+			}
+			translationBuilder.add("tooltip.inksoul.ct_machine", "Just a fake machine.");
+		}
+	}
+
+	private static class ChineseLanguageGenerator extends FabricLanguageProvider {
+		private ChineseLanguageGenerator(FabricDataGenerator dataGenerator) {
+			// Specifying en_us is optional, by default is is en_us.
+			super(dataGenerator, "zh_cn");
+		}
+
+		@Override
+		public void generateTranslations(TranslationBuilder translationBuilder) {
+			for (var i : ExampleMod.iiitem) {
+				translationBuilder.add(i, "机器方块");
+			}
+			translationBuilder.add("tooltip.inksoul.ct_machine", "只是一台假的机器而已");
+		}
 	}
 }
